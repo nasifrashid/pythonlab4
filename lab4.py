bc4aeba26623b9a543f788931170f01b9aed3854
Nasif Rashid <nasif@dal.ca>
2020-05-11 16:01:02 -0300

    Attempted Task 1

--- lab4.py
+++ lab4.py
@@ -1,2 +1,20 @@
-#print("hello")
+# print("hello")
+from string import punctuation, whitespace
 
+fin = open('pnp.txt')
+for line in fin:
+    word = line.strip()
+    print(word)
+
+
+def strip(words):
+    stripped = ''
+    for char in words:
+        if ((char in punctuation) or (char in whitespace)):
+            pass
+        else:
+            stripped += char.lower()
+    return stripped
+
+
+print(word)
