7d273dd74b78ab677dcb593db3109b25f49f19cc
Nasif Rashid <nasif@dal.ca>
2020-05-13 02:50:03 -0300

    OS problem solution from thinpython has been added

---
+++ lab4_5.py
@@ -0,0 +1,17 @@
+def sed(pattern, replacement, readfile, writefile):
+    try:
+        r = open(readfile)
+        w = open(writefile, 'w')
+        a = 'a'
+        text = ''
+        while a != '':
+            a = r.readline()
+            text += a
+        while pattern in text:
+            p = text.find(pattern)
+            text = text[:p] + replacement + text[p + len(pattern):]
+        w.write(text)
+        r.close()
+        w.close()
+    except:
+        return 'error message'
